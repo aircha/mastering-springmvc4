a1985fcf378ba3cdaf33f051a8721fc70a451ad5
jhcha <jhcha@koticobui.com>
2016-09-18 10:52:19 +0900

    초기 소스 gradle 빌드

---
+++ src/main/java/kr/co/hanbit/mastering/MasteringSpirngMvc4Application.java
@@ -0,0 +1,12 @@
+package kr.co.hanbit.mastering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteringSpirngMvc4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasteringSpirngMvc4Application.class, args);
+	}
+}
